fd594953aa5351fddb989f9cd69a8abd0eccd361
Alankazamm <bruno.alanc61@gmail.com>
2023-02-05 18:25:27 -0300

    redux-toolkit example

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,49 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { divActions } from './store/reducerFunction';
-
+import { useDispatch, useSelector } from "react-redux";
+import { divActions } from "./store/reducerFunction";
 const App = () => {
-  const width = useSelector((state) => state.width);
-  const height = useSelector((state) => state.height);
+	const width = useSelector((state) => state.width);
+	const height = useSelector((state) => state.height);
 
-  const dispatch = useDispatch();
-  const clickHandler = (e) => {
-    switch (e.target.id) {
-      case 'increaseWidth':
-        dispatch(divActions.increaseWidth(20));
-        break;
-      case 'decreaseWidth':
-        dispatch(divActions.decreaseWidth(10));
-        break;
-      case 'increaseHeight':
-        dispatch(divActions.increaseHeight(20));
-        break;
-    }
-  }
+	const dispatch = useDispatch();
 
-  return (
-    <>
-      <div style={{
-        background: 'teal',
-        height: height,
-        width: width
-      }}>
-      </div>
-      <div style={{ marginTop: '3rem' }}>
-        <button id="increaseWidth"
-          onClick={clickHandler}>
-          Increase bar width
-        </button>
+	const barChangeHandler = (e) => {
+		switch (e.target.id) {
+			case "increase-width":
+				dispatch(divActions.increaseWidth(20));
+				break;
+			case "decrease-width":
+				dispatch(divActions.decreaseWidth(10));
+				break;
+			case "increase-height":
+				dispatch(divActions.increaseHeight(20));
+				break;
+		}
+	};
 
-        <button id="decreaseWidth"
-          onClick={clickHandler}>
-          Decrease bar width
-        </button>
-        <button id="increaseHeight"
-          onClick={clickHandler}>
-          Increase bar height
-        </button>
-      </div>
-    </>
-  );
-  
-}
+	return (
+		<>
+			<div
+				style={{
+					background: "teal",
+					height: height,
+					width: width,
+				}}
+			></div>
+
+			<div style={{ marginTop: "3rem" }}>
+				<button id="increase-width" onClick={barChangeHandler}>
+					Increase bar width
+				</button>
+
+				<button id="decrease-width" onClick={barChangeHandler}>
+					Decrease bar width
+				</button>
+				<button id="increase-height" onClick={barChangeHandler}>
+					Increase bar height
+				</button>
+			</div>
+		</>
+	);
+};
 
 export default App;
--- src/store/reducerFunction.jsx
+++ src/store/reducerFunction.jsx
@@ -1,27 +1,28 @@
+import { createSlice, configureStore } from "@reduxjs/toolkit";
+const initialState = { width: 40, height: 20 };
 
-import { createSlice, configureStore } from '@reduxjs/toolkit';
-
-const initialState = { width: 40, height: 40 };
-
-const divSlice = createSlice(
-    {
-        name: 'divChange',
-        initialState: initialState,
-        reducers: {
-            increaseWidth(state, action) {
-                state.width = state.width + action.payload
-            },
-            decreaseWidth(state, action) {
-               state.width = state.width - action.payload
-            },
-            increaseHeight(state, action) {
-                state.height = state.height + action.payload
-            }
-        }
-    }
-)
+//TODO actions manager(createSlice)
+const divSlice = createSlice({
+	name: "divChange",
+	initialState: initialState,
+	reducers: {
+		increaseWidth(state, action) {
+			state.width = state.width + action.payload;
+		},
+		decreaseWidth(state, action) {
+			state.width = state.width - action.payload;
+		},
+		increaseHeight(state, action) {
+			state.height = state.height + action.payload;
+		},
+	},
+});
+//TODO store our actions from the slice object
 export const divActions = divSlice.actions;
 
+//TODO create our 'store' which gonna be used by the Provider
+//(with configureStore)
+
 export const store = configureStore({
-    reducer: divSlice.reducer,
-})
+	reducer: divSlice.reducer,
+});
